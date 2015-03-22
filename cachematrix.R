6c51dadab176ca52741ea93d236e130b56d98dc5
leonardo catani <leonardocatani@gmail.com>
2015-03-23 00:17:55 +0100

    Catani assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,21 @@
 makeCacheMatrix <- function(x = matrix()) {
 
         m <- NULL
+	## set the value of the matrix
+
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+	## get the value of the matrix
         get <- function() x
-        setinverse <- function(solve) m <<- solve
+       
+	## set the inverse of the matrix
+ setinverse <- function(solve) m <<- solve
+
+	## get the inverse of the matrix 
         getinverse <- function() m
+
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -26,10 +34,13 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
         m <-x$getinverse()
+	
+	## check if the inverse has already been calculated
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+	## if the inverse has not been calculated it provide the computation
         data <- x$get()
         m <- solve(data, ...)
         x$setinverse(m)
